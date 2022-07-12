624485e3cb2bf9835500e515d0e7dba71307e38e
jgcapdev <jorge.gomez-carrillo-external@capgemini.com>
2022-07-12 12:43:27 +0200

    feat: adding lights and textures

--- src/app/models/floor/floor.component.ts
+++ src/app/models/floor/floor.component.ts
@@ -1,16 +1,75 @@
-import { NgtEuler, NgtVector3 } from '@angular-three/core';
+import { NgtEuler, NgtVector3, NgtLoader } from '@angular-three/core';
 import { Component, OnInit, Input } from '@angular/core';
+import { NgtTextureLoader } from '@angular-three/soba/loaders';
+import { MeshStandardMaterialParameters, Texture, TextureLoader } from 'three';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-floor',
   templateUrl: './floor.component.html',
   styleUrls: ['./floor.component.css'],
+  providers: [NgtTextureLoader],
 })
 export class FloorComponent implements OnInit {
   @Input() position?: NgtVector3;
   @Input() rotation?: NgtEuler;
 
-  constructor() {}
+  constructor(
+    private textureLoader: NgtTextureLoader,
+    private loader: NgtLoader
+  ) {}
 
   ngOnInit(): void {}
+
+  texture!: Texture;
+  textureNormal!: Texture;
+  textureAo!: Texture;
+  textureRn!: Texture;
+
+  texture$ = this.loader
+    .use(TextureLoader, 'assets/grass/color.jpg')
+    .subscribe((text) => {
+      text.wrapS = THREE.RepeatWrapping;
+      text.wrapT = THREE.RepeatWrapping;
+      text.repeat.set(8, 8);
+
+      this.texture = text;
+    });
+
+  textureNormal$ = this.loader
+    .use(TextureLoader, 'assets/grass/normal.jpg')
+    .subscribe((text) => {
+      text.wrapS = THREE.RepeatWrapping;
+      text.wrapT = THREE.RepeatWrapping;
+      text.repeat.set(8, 8);
+
+      this.textureNormal = text;
+    });
+
+  textureAo$ = this.loader
+    .use(TextureLoader, 'assets/grass/ambientOcclusion.jpg')
+    .subscribe((text) => {
+      text.wrapS = THREE.RepeatWrapping;
+      text.wrapT = THREE.RepeatWrapping;
+      text.repeat.set(8, 8);
+    });
+
+  textureRoughness = this.loader
+    .use(TextureLoader, 'assets/grass/roughness.jpg')
+    .subscribe((text) => {
+      text.wrapS = THREE.RepeatWrapping;
+      text.wrapT = THREE.RepeatWrapping;
+      text.repeat.set(8, 8);
+    });
+
+  get parameters(): MeshStandardMaterialParameters {
+    const p: MeshStandardMaterialParameters = {};
+
+    p.map = this.texture;
+    p.aoMap = this.textureAo;
+    p.normalMap = this.textureNormal;
+    p.roughnessMap = this.textureRn;
+
+    return p;
+  }
 }
--- src/app/models/standard-room/standard-room.component.ts
+++ src/app/models/standard-room/standard-room.component.ts
@@ -1,12 +1,14 @@
-import { NgtRenderState, NgtVector3 } from '@angular-three/core';
+import { NgtRenderState, NgtVector3, NgtLoader } from '@angular-three/core';
 import { Component, Input, OnInit } from '@angular/core';
 import { Mesh } from 'three';
 import * as THREE from 'three';
+import { NgtTextureLoader } from '@angular-three/soba/loaders';
 
 @Component({
   selector: 'app-standard-room',
   templateUrl: './standard-room.component.html',
   styleUrls: ['./standard-room.component.css'],
+  providers: [NgtLoader, NgtTextureLoader],
 })
 export class StandardRoomComponent implements OnInit {
   @Input() position?: NgtVector3;
@@ -14,10 +16,19 @@ export class StandardRoomComponent implements OnInit {
   @Input() scale?: NgtVector3;
   @Input() wireframe?: boolean;
 
+  readonly texture$ = this.textureLoader.load('assets/bricks/color.jpg');
+  readonly textureAo$ = this.textureLoader.load(
+    'assets/bricks/ambientOcclusion.jpg'
+  );
+  readonly textureNormal$ = this.textureLoader.load('assets/bricks/normal.jpg');
+  readonly textureRoughness$ = this.textureLoader.load(
+    'assets/bricks/roughness.jpg'
+  );
+
   size: any = THREE.DoubleSide;
   transparents = ['true', 'false', 'true', 'false', 'true', 'false'];
 
-  constructor() {}
+  constructor(private textureLoader: NgtTextureLoader) {}
 
   ngOnInit(): void {}
 
